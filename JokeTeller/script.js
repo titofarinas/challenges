1b9d2f870667c848227757537afe61c988187aa6
tfarinas <titofarinas@outlook.com>
2022-11-17 16:45:21 -0600

    adding voiceRSS.js

--- JokeTeller/script.js
+++ JokeTeller/script.js
@@ -1,1 +1,20 @@
-console.log('testing');
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
+
+
+const api = '15b64b8304074d6a973483cbb5f65694';
+
+function test() {
+    VoiceRSS.speech({
+        key: api,
+        src: 'Turno 25, ventanilla 1',
+        hl: 'es-es',
+        v: 'Linda',
+        r: 0, 
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });  
+}
+
+test();
